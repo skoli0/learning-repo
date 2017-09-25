ffba91d22ec7784f8a1155d1a2f3ad4b3c43ee9e
Sandeep Koli <sandeep.akoli@gmail.com>
2017-09-25 19:26:38 +0530

    program to read itself and print output on screen

---
+++ c/readme.c
@@ -0,0 +1,17 @@
+// Program to read itself and print it on screen
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char c;
+
+    fp = fopen(__FILE__, "r");
+
+    do {
+        c = getc(fp);
+        putchar(c);
+    } while (c != EOF);
+
+    return 0;
+}

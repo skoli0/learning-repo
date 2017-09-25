32278f67bcc063bf6aa34e059ff0c924e978d2ff
Sandeep Koli <sandeep.akoli@gmail.com>
2017-09-25 19:15:45 +0530

    program to find if linked list is circular

---
+++ c/circular_list.c
@@ -0,0 +1,59 @@
+/* C Program to check whether linked list is circular or not */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *new_node(int item) {
+    struct node *Node = (struct node *) malloc(sizeof(struct node));
+    Node->data = item;
+    Node->next = NULL;
+
+    return Node;
+}
+
+void printlist(struct node* head)
+{
+    struct node *temp = head;
+
+    if (head == NULL)
+        printf("List is empty\n");
+    else
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+    printf("\n");
+}
+
+int iscircular(struct node *head)
+{
+    struct node *temp = head->next;
+
+    if (head == NULL)
+        return 0;
+
+    while (temp != NULL && temp != head) {
+        temp = temp->next;
+    }
+    return (temp == head);
+}
+
+int main()
+{
+    struct node *head = new_node(1);
+    //printf("%d %p\n", head->data, head->next);
+    head->next = new_node(2);
+    //printf("%d %p\n", head->next->data, head->next);
+    head->next->next = new_node(3);
+    head->next->next->next = head;
+
+    //printlist(head);
+
+    iscircular(head) ? printf("Yes, it is circular.\n") :
+                       printf("No, it's not circular.\n");
+    return 0;
+}
